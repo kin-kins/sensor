095c0071bd55018ff4d7e465eebc08e0cef41f25
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-06-02 00:32:04 +0530

    Add files via upload

---
+++ sevo.ino
@@ -0,0 +1,17 @@
+#include<Servo.h>
+int servopin=4;
+Servo Servo1;
+void setup() {
+  // put your setup code here, to run once:
+Servo1.attach(servopin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+Servo1.write(0);
+delay(1000);
+Servo1.write(180);
+delay(1000);
+
+}
