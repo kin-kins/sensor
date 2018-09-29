3d24d9aba00eeadf41932e9d3a72191cb8ae2966
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-09-29 17:02:10 +0530

    Create water_level_meter.ino

---
+++ water_level_meter.ino
@@ -0,0 +1,47 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(3,OUTPUT);
+pinMode(4,INPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+Serial.begin(9600);
+}
+void setup() {
+  // put your setup code here, to run once:
+pinMode(3,OUTPUT);
+pinMode(4,INPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(3,HIGH);
+digitalWrite(4,HIGH);
+int c =digitalRead(4);
+Serial.println(c);
+if(c==0)
+{digitalWrite(7,HIGH);
+digitalWrite(8,LOW);
+delay(10);
+}
+else
+digitalWrite(7,0);
+delay(500);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(3,HIGH);
+digitalWrite(4,HIGH);
+int c =digitalRead(4);
+Serial.println(c);
+if(c==0)
+{digitalWrite(7,HIGH);
+digitalWrite(8,LOW);
+delay(10);
+}
+else
+digitalWrite(7,0);
+delay(500);
+}
