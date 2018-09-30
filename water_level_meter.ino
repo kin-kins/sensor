79ca5110f963d69435c7d54daef0426419dca1d8
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-09-30 17:54:12 +0530

    Update water_level_meter.ino

--- water_level_meter.ino
+++ water_level_meter.ino
@@ -2,16 +2,15 @@ void setup() {
   // put your setup code here, to run once:
 pinMode(3,OUTPUT);
 pinMode(4,INPUT);
+pinMode(5,INPUT);
+pinMode(6,INPUT);
 pinMode(7,OUTPUT);
 pinMode(8,OUTPUT);
-Serial.begin(9600);
-}
-void setup() {
-  // put your setup code here, to run once:
-pinMode(3,OUTPUT);
-pinMode(4,INPUT);
-pinMode(7,OUTPUT);
-pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+
 Serial.begin(9600);
 }
 
@@ -19,8 +18,14 @@ void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(3,HIGH);
 digitalWrite(4,HIGH);
+digitalWrite(5,HIGH);
+digitalWrite(6,HIGH);
 int c =digitalRead(4);
-Serial.println(c);
+int d =digitalRead(5);
+int e =digitalRead(6);
+
+
+
 if(c==0)
 {digitalWrite(7,HIGH);
 digitalWrite(8,LOW);
@@ -28,20 +33,34 @@ delay(10);
 }
 else
 digitalWrite(7,0);
-delay(500);
-}
-void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(3,HIGH);
-digitalWrite(4,HIGH);
-int c =digitalRead(4);
-Serial.println(c);
-if(c==0)
-{digitalWrite(7,HIGH);
-digitalWrite(8,LOW);
+//delay(500);
+
+
+if(d==0)
+{digitalWrite(9,HIGH);
+digitalWrite(10,LOW);
 delay(10);
 }
 else
-digitalWrite(7,0);
+digitalWrite(9,0);
+
+
+
+
+if(e==0)
+{digitalWrite(11,HIGH);
+digitalWrite(12,LOW);
+delay(10);
+}
+else
+digitalWrite(11,0);
 delay(500);
+
+
+Serial.print("HOUSE----- ");
+Serial.println(c);
+Serial.print("PIPELINE-- ");
+Serial.println(d);
+Serial.print("TANK------ ");
+Serial.println(e);
 }
